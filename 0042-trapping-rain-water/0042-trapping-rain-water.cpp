c82dbb72e0c10afb71c594f440adb2f6ceb3d2c5
Sanyam302 <146521586+Sanyam302@users.noreply.github.com>
2025-07-04 22:31:09 +0530

    Time: 0 ms (100%), Space: 26 MB (79.4%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -5,21 +5,26 @@ public:
         int leftMax = 0, rightMax = 0;
         int ans = 0;
 
-        while (left < right) {
-            if (height[left] < height[right]) {
-                if (height[left] >= leftMax)
-                    leftMax = height[left];
-                else
-                    ans += leftMax - height[left];
-                left++;
-            } else {
-                if (height[right] >= rightMax)
-                    rightMax = height[right];
-                else
-                    ans += rightMax - height[right];
-                right--;
+       while(left<right){
+        if(height[left]<height[right]){
+            if(height[left]>leftMax){
+                leftMax=height[left];
             }
+            else{
+                ans+=leftMax-height[left];
+            }
+            left++;
         }
+        else{
+            if(rightMax<height[right]){
+              rightMax=height[right];
+            }
+            else{
+                ans+=rightMax-height[right];
+            }
+            right--;
+        }
+       }
 
         return ans;
     }
