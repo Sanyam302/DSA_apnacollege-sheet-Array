44bc4125ee3aa23c9d9822dcdc4e78d19f866465
Sanyam302 <146521586+Sanyam302@users.noreply.github.com>
2025-07-11 23:34:25 +0530

    Time: 47 ms (71.34%), Space: 29.1 MB (51.16%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& arr) {
-        vector<vector<int>>ans;
-        set<vector<int>>s;
-        
-        int n=arr.size();
-        sort(arr.begin(),arr.end());
-      for(int i=0;i<n-2;i++){
-        int a=i+1;
-        int b=n-1;
-        while(a<b){
-            if(arr[a]+arr[b]==-arr[i]){
-                vector<int>t={arr[i],arr[a],arr[b]};
-                if(s.find(t)==s.end()){
-                    s.insert(t);
-                    ans.push_back(t);
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate values for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int a = i + 1;
+            int b = n - 1;
+
+            while (a < b) {
+                int sum = nums[i] + nums[a] + nums[b];
+
+                if (sum == 0) {
+                    ans.push_back({nums[i], nums[a], nums[b]});
+                    a++;
+                    b--;
+
+                    // Skip duplicates for a and b
+                    while (a < b && nums[a] == nums[a - 1]) a++;
+                    while (a < b && nums[b] == nums[b + 1]) b--;
+
+                } else if (sum < 0) {
+                    a++;
+                } else {
+                    b--;
                 }
-                a++;
-                b--;
-                
             }
-            else if(arr[a]+arr[b]>-arr[i]) b--;
-                else a++;
         }
-      }  
-      return ans;
+
+        return ans;
     }
 };
