80ea318ec3be0f7b263c54c373d7e298346a6304
Sanyam302 <146521586+Sanyam302@users.noreply.github.com>
2025-07-11 23:48:02 +0530

    Time: 39 ms (95.56%), Space: 29 MB (90.17%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> ans;
-        if (n < 3) return ans;
         sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        int n = nums.size();
 
         for (int i = 0; i < n - 2; ++i) {
-            // If nums[i] is positive, sum can't be zero any more
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             if (nums[i] > 0) break;
 
-            // Skip duplicate 'i' values
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1, right = n - 1;
 
-            int l = i + 1, r = n - 1;
-            while (l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0) {
-                    ans.push_back({nums[i], nums[l], nums[r]});
-                    ++l; --r;
-                    // Skip duplicates for l and r
-                    while (l < r && nums[l] == nums[l - 1]) ++l;
-                    while (l < r && nums[r] == nums[r + 1]) --r;
-                }
-                else if (sum < 0) {
-                    ++l;
+                    res.push_back({nums[i], nums[left], nums[right]});
+                    ++left;
+                    --right;
+                    while (left < right && nums[left] == nums[left - 1]) ++left;
+                    while (left < right && nums[right] == nums[right + 1]) --right;
+                } else if (sum < 0) {
+                    ++left;
                 } else {
-                    --r;
+                    --right;
                 }
             }
         }
-        return ans;
+
+        return res;
     }
 };
 
