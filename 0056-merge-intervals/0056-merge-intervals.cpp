c3b2a6ac7d560c4b1c1ec066f9cb25b9ae6e5667
Sanyam302 <146521586+Sanyam302@users.noreply.github.com>
2025-07-12 18:26:28 +0530

    Time: 129 ms (5.04%), Space: 52.8 MB (5.08%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,25 +3,25 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& arr) {
         if(arr.empty()) return arr;
         
-        sort(arr.begin(),arr.end(),[](vector<int>a,vector<int>b){
-            return a[0]<b[0];
+        // Sort intervals by starting time
+        sort(arr.begin(), arr.end(), [](vector<int> a, vector<int> b) {
+            return a[0] < b[0];
         });
-        vector<vector<int>>ans;
+
+        vector<vector<int>> ans;
         ans.push_back(arr[0]);
-        
-        
-        for(int i=1;i<arr.size();i++){
-           vector<int>&l=ans.back();
-           if(arr[i][0]<=l[1]){
-            l[1]=max(arr[i][1],l[1]);
-           }
-            
-            else{
-            ans.push_back(arr[i]);
-        } 
+
+        for (int i = 1; i < arr.size(); i++) {
+            vector<int>& l = ans.back();
+            if (arr[i][0] <= l[1]) {
+                // Overlap: merge intervals
+                l[1] = max(arr[i][1], l[1]);
+            } else {
+                // No overlap: push new interval
+                ans.push_back(arr[i]);
+            }
         }
-       
-        
+
         return ans;
     }
 };
