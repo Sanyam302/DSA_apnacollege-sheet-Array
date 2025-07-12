4132aac2f99c20279cd22a7030444231d64f78ed
Sanyam302 <146521586+Sanyam302@users.noreply.github.com>
2025-07-12 18:20:59 +0530

    Time: 119 ms (6.3%), Space: 52.6 MB (5.08%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& arr) {
-        if(arr.empty()) return arr;
-        if(arr.size()==1) return arr;
+        if(arr.empty()||arr.size()==1) return arr;
+        
         sort(arr.begin(),arr.end(),[](vector<int>a,vector<int>b){
             return a[0]<b[0];
         });
         vector<vector<int>>ans;
-        ans.push_back({arr[0][0],arr[0][1]});
+        ans.push_back(arr[0]);
         int n=arr.size();
         
         for(int i=0;i<n;i++){
