92ff522969d6fdaefabd84874e69108c3691fe03
Sanyam302 <146521586+Sanyam302@users.noreply.github.com>
2025-07-12 18:25:52 +0530

    Time: 127 ms (5.36%), Space: 52.6 MB (5.08%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> ans;
-
-        if (intervals.empty()) return ans;
-
-        // Step 1: Sort by start time
-        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) {
-            return a[0] < b[0];
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        if(arr.empty()) return arr;
+        
+        sort(arr.begin(),arr.end(),[](vector<int>a,vector<int>b){
+            return a[0]<b[0];
         });
-
-        // Step 2: Start with the first interval
-        ans.push_back(intervals[0]);
-
-        for (int i = 1; i < intervals.size(); i++) {
-            vector<int>& last = ans.back();  // last merged interval
-
-            if (intervals[i][0] <= last[1]) {
-                // Overlapping → merge
-                last[1] = max(last[1], intervals[i][1]);
-            } else {
-                // No overlap → push new interval
-                ans.push_back(intervals[i]);
-            }
+        vector<vector<int>>ans;
+        ans.push_back(arr[0]);
+        
+        
+        for(int i=1;i<arr.size();i++){
+           vector<int>&l=ans.back();
+           if(arr[i][0]<=l[1]){
+            l[1]=max(arr[i][1],l[1]);
+           }
+            
+            else{
+            ans.push_back(arr[i]);
+        } 
         }
-
+       
+        
         return ans;
     }
 };
