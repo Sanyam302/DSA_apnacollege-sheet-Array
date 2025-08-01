9ab3ebd51315578465415bb630a5b3a6397129a8
Sanyam302 <146521586+Sanyam302@users.noreply.github.com>
2025-08-02 03:56:44 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,8 +2,12 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length()) return 0;
-       sort(s.begin(),s.end());
-       sort(t.begin(),t.end());
-       return s==t;
+       vector<int>count(26,0);
+       vector<int>count2(26,0);
+       for(int i=0;i<s.length();i++){
+        count[s[i]-'a']++;
+        count2[t[i]-'a']++;
+       }
+       return count==count2;
     }
 };
