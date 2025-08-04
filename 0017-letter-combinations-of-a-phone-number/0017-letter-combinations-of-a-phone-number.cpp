2d118ac9800a28581bc52c7f9f588b28a8982633
Sanyam302 <146521586+Sanyam302@users.noreply.github.com>
2025-08-04 13:24:00 +0530

    Time: 0 ms (100%), Space: 9.3 MB (40.22%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,29 +1,46 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        if (digits.empty()) return {};
+        if(digits.empty()) return {};
+        vector<string>str(10);
 
-        vector<string> str(10);
-        int a = 97;
-        for (int i = 2; i <= 9; i++) {
-            int letters = (i == 7 || i == 9) ? 4 : 3;
-            for (int j = 0; j < letters; j++) {
-                str[i] += char(a++);
-            }
+        str[0]="";
+        str[1]="";
+        int a=97;
+        for(int i=2;i<=9;i++){
+         
+          if(i==7||i==9){
+          str[i]+= char(a++);  
+          str[i]+= char(a++);  
+          str[i]+= char(a++);
+          str[i]+= char(a++);
+          }  
+          else{
+             str[i]+= char(a++);  
+          str[i]+= char(a++);  
+          str[i]+= char(a++);
+          }
+
+        }
+         vector<string>result={""};
+        
+        vector<string>d;
+        for(int i=0;i<digits.size();i++){
+           d.push_back(str[digits[i] - '0']);
         }
 
-        vector<string> result = {""};
-
-        for (char ch : digits) {
-            vector<string> temp;
-            string letters = str[ch - '0'];
-            for (string prefix : result) {
-                for (char c : letters) {
-                    temp.push_back(prefix + c);
-                }
+       
+       for(int i=0;i<d.size();i++){
+        string s= d[i];
+        vector<string>temp;
+        for(string prefix:result){
+            for(char a:s){
+                temp.push_back(prefix+a);
             }
-            result = temp;
+            
         }
-        return result;
+        result=temp;
+       }
+       return result;
     }
 };
