d40514b3e092a61650c31a225ea238bce9934007
Sanyam302 <146521586+Sanyam302@users.noreply.github.com>
2025-08-11 14:59:56 +0530

    Time: 3 ms (83.22%), Space: 11.6 MB (43.78%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,44 +2,47 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         if (s.size() < t.size()) return "";
-        unordered_map<char, int> tfreq;
-        for (char c : t) tfreq[c]++;
-        
-        unordered_map<char, int> windowCounts;
-        int required = tfreq.size();  // number of unique chars to be matched
-        int formed = 0;                // how many unique chars currently meet requirement
-        
+
+        int tfreq[128] = {0};
+        int sfreq[128] = {0};
+        int required = 0; // unique chars in t
+
+        // Count frequencies in t and number of unique chars
+        for (char c : t) {
+            if (tfreq[c] == 0) required++;
+            tfreq[c]++;
+        }
+
+        int formed = 0;
         int left = 0, right = 0;
-        int minLen = INT_MAX, minStart = 0;
-        
+        int minLen = INT_MAX;
+        int minStart = 0;
+
         while (right < s.size()) {
             char c = s[right];
-            windowCounts[c]++;
-            
-            if (tfreq.find(c) != tfreq.end() && windowCounts[c] == tfreq[c]) {
+            sfreq[c]++;
+
+            // Check if this char frequency matches required freq
+            if (tfreq[c] != 0 && sfreq[c] == tfreq[c]) {
                 formed++;
             }
-            
-            // Try to shrink the window from the left if all chars matched
+
+            // Shrink window while valid
             while (left <= right && formed == required) {
-                // Update answer if smaller window found
                 if (right - left + 1 < minLen) {
                     minLen = right - left + 1;
                     minStart = left;
                 }
-                
-                // Remove leftmost char from window
                 char leftChar = s[left];
-                windowCounts[leftChar]--;
-                if (tfreq.find(leftChar) != tfreq.end() && windowCounts[leftChar] < tfreq[leftChar]) {
+                sfreq[leftChar]--;
+                if (tfreq[leftChar] != 0 && sfreq[leftChar] < tfreq[leftChar]) {
                     formed--;
                 }
                 left++;
             }
-            
             right++;
         }
-        
+
         return (minLen == INT_MAX) ? "" : s.substr(minStart, minLen);
     }
 };
