15a636fe2a64598894760c5f6d4585b5612321b6
Sanyam302 <146521586+Sanyam302@users.noreply.github.com>
2025-08-11 10:51:20 +0530

    Time: 0 ms (100%), Space: 15.7 MB (67.38%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n=nums.size();
-        int pivot=-1;
-     for(int i=n-1;i>0;i--){
-        if(nums[i]>nums[i-1])
-        { pivot=i-1;
-        break;
-        }
+        int n = nums.size();
+        if(n <= 1) return;
 
-     }
-    
-     if(pivot==-1){
-        reverse(nums.begin(),nums.end());
-        return;
+        int pivot = -1;
 
-     }  
-     else{
-       int swapIndex = -1;
-        for (int i = n - 1; i > pivot; i--) {
-            if (nums[i] > nums[pivot]) {
-                swapIndex = i;
+        // Step 1: Find pivot (rightmost element smaller than its next)
+        for(int i = n-1; i > 0; i--){
+            if(nums[i] > nums[i-1]){
+                pivot = i-1;
                 break;
             }
         }
-    swap(nums[pivot],nums[swapIndex]);
-        reverse(nums.begin()+pivot+1,nums.end());
-     }
+
+        // Step 2: If no pivot found, reverse (last permutation → first permutation)
+        if(pivot == -1){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        // Step 3: Find the rightmost element greater than pivot
+        for(int i = n-1; i > pivot; i--){
+            if(nums[i] > nums[pivot]){
+                swap(nums[i], nums[pivot]);
+                break;
+            }
+        }
+
+        // Step 4: Reverse the suffix
+        reverse(nums.begin() + pivot + 1, nums.end());
     }
 };
