556cec4966f85e1116b6e9b0974b4cffd18e6933
Sanyam302 <146521586+Sanyam302@users.noreply.github.com>
2025-08-13 23:43:37 +0530

    Time: 3 ms (55.71%), Space: 14 MB (72.75%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    void subSet(vector<int> &arr, int target, vector<int> &temp, int i, int n, vector<vector<int>> &ans) {
-        if (target == 0) {
-            ans.push_back(temp);
+    void helper(vector<int>& candidates, int target,int idx,vector<int>&temp,vector<vector<int>>&result){
+        if(target==0){
+            result.push_back(temp);
             return;
         }
-        if (i == n || target < 0) return;
+        for(int i=idx;i<candidates.size();i++){
+            if(candidates[i] > target) break;
 
-        // Include arr[i] (can reuse)
-        temp.push_back(arr[i]);
-        subSet(arr, target - arr[i], temp, i, n, ans); // stay at i to allow reuse
-        temp.pop_back();
-
-        // Exclude arr[i]
-        subSet(arr, target, temp, i + 1, n, ans); // move to next
+            temp.push_back(candidates[i]);
+            helper(candidates,target-candidates[i],i,temp,result); //pas the same i th element as it can be used infinite times till it is not equal to target also decrement the target 
+            temp.pop_back();
+        }
     }
-
-    vector<vector<int>> combinationSum(vector<int> &arr, int target) {
-        vector<int> temp;
-        vector<vector<int>> ans;
-        sort(arr.begin(), arr.end());
-        arr.erase(unique(arr.begin(), arr.end()), arr.end()); // remove duplicate numbers
-        subSet(arr, target, temp, 0, arr.size(), ans);
-        return ans;
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>result;
+        vector<int>temp;
+        sort(candidates.begin(),candidates.end());
+        helper(candidates,target,0,temp,result);
+        return result;
     }
 };
