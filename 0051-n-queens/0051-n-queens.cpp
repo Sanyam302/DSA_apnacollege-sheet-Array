7509aa710faecdd22eb664b5d5876c8645f38a2c
Sanyam302 <146521586+Sanyam302@users.noreply.github.com>
2025-08-14 22:23:54 +0530

    Time: 3 ms (65.68%), Space: 11.8 MB (25.81%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+bool issafe(int i,int j,vector<vector<int>> &placed){
+   int a=placed.size();
+      if(!placed.empty() ){
+        for(int x=0;x<a;x++)
+       { if(i==placed[x][0] || j==placed[x][1]){
+            return false;
+        }
+        if(abs(placed[x][0]-i)==abs(placed[x][1]-j)){
+            return false;
+        }}
+ }
+      return true;
+}
+void Nqueens(int n,vector<vector<string>>&ans,int i,vector<vector<int>>&placed,vector<string>&temp){
+    if(i==n){
+        ans.push_back(temp);
+        return;
+    }
+      
+      for(int col=0;col<n;col++){
+           if(issafe(i,col,placed)){
+             placed.push_back({i,col});
+             string block="";
+              for(int x=0;x<n;x++){
+                if(x==col){
+                    block+="Q";
+                }
+                else{
+                    block+=".";
+                }
+              }
+              temp.push_back(block);
+              int row=i+1;
+              Nqueens(n,ans,row,placed,temp);
+              placed.pop_back();
+              temp.pop_back();
+              }
+      }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+           vector<vector<string>>ans;
+        vector<vector<int>>placed;
+        vector<string>temp;
+
+         Nqueens(n,ans,0,placed,temp);
+         return ans;
+        
+    }
+};
