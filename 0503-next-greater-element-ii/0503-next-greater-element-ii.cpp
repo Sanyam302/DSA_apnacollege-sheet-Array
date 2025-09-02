d114d1c4e32ebc9886c6dc85614534678bde61c9
Sanyam302 <146521586+Sanyam302@users.noreply.github.com>
2025-09-02 21:45:17 +0530

    Time: 3 ms (75.41%), Space: 28.9 MB (16.61%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -3,20 +3,24 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         stack<int> s;
         int n = nums.size();
-        vector<int> ans(n, -1);   // store answers aligned to indices
+        vector<int> ans(n, -1);
+        
+        for(int j = n - 1; j >= 0; j-- ){
+            s.push(nums[j]);
+        }  
 
-        for (int j = 2*n - 1; j >= 0; j--) {
-            int idx = j % n;
+        for (int j = n - 1; j >= 0; j--) {
+            int idx = j;
 
             // pop all smaller/equal elements
             while (!s.empty() && s.top() <= nums[idx]) {
                 s.pop();
             }
 
-            if (j < n) { // only fill results in first pass
+            // only fill results in first pass
                 if (!s.empty()) ans[idx] = s.top();
-                else ans[idx] = -1;
-            }
+            
+            
 
             s.push(nums[idx]);
         }
